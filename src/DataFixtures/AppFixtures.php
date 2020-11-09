e4c859e275bd76bb5eaa6b8ea8682c8d7b8e612a
benaor <benjamingirard25@gmail.com>
2020-11-09 23:31:59 +0100

    Create fake datafixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
+use App\Entity\Invoice;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+
+        for($c=0 ; $c<30 ; $c++){
+
+            $customer = new Customer(); 
+            $customer->setFirstName($faker->firstName())
+                     ->setLastName($faker->lastName)
+                     ->setCompany($faker->company)
+                     ->setEmail($faker->email);
+            
+            $manager->persist($customer);
+
+            for ($i=0; $i < mt_rand(3, 20) ; $i++) { 
+
+                $invoice = new Invoice(); 
+                $invoice->setAmout($faker->randomFloat(2, 250, 5000))
+                        ->setSentAt($faker->dateTimeBetween('-6 months'))
+                        ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
+                        ->setCustomer($customer); 
+
+                $manager->persist($invoice);
+            }
+        }
 
         $manager->flush();
     }
