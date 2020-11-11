d7faa21e501f11136fb2f3c6245340ddbe457866
benaor <benjamingirard25@gmail.com>
2020-11-12 00:02:58 +0100

    include loop of creating Costumer in loop of creating User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -17,46 +17,46 @@ class AppFixtures extends Fixture
         $faker  = Factory::create('fr_FR');
         $chrono = 1;
 
-        $benjamin = new User();
-        $benjamin->setFirstName("Benjamin")
-                 ->setLastName("Girard")
-                 ->setEmail("benjamin@girard.com")
-                 ->setPassword("password");
+        for ($u = 0; $u < 10; $u++) {
 
-        $manager->persist($benjamin);
+            $benjamin = new User();
+            $benjamin->setFirstName("Benjamin")
+                ->setLastName("Girard")
+                ->setEmail("benjamin@girard.com")
+                ->setPassword("password");
+            $manager->persist($benjamin);
 
-        for ($u=0; $u < 10; $u++) { 
             $user = new User();
             $user->setFirstName($faker->firstName())
-                 ->setLastName($faker->lastName)
-                 ->setEmail($faker->email)
-                 ->setPassword("password");
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword("password");
 
             $manager->persist($user);
-        }
 
-        for($c=0 ; $c<30 ; $c++){
+            for ($c = 0; $c < 30; $c++) {
 
-            $customer = new Customer(); 
-            $customer->setFirstName($faker->firstName())
-                     ->setLastName($faker->lastName)
-                     ->setCompany($faker->company)
-                     ->setEmail($faker->email);
-            
-            $manager->persist($customer);
+                $customer = new Customer();
+                $customer->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email);
 
-            for ($i=0; $i < mt_rand(3, 20) ; $i++) { 
+                $manager->persist($customer);
 
-                $invoice = new Invoice(); 
-                $invoice->setAmout($faker->randomFloat(2, 250, 5000))
+                for ($i = 0; $i < mt_rand(3, 20); $i++) {
+
+                    $invoice = new Invoice();
+                    $invoice->setAmout($faker->randomFloat(2, 250, 5000))
                         ->setSentAt($faker->dateTimeBetween('-6 months'))
                         ->setStatus($faker->randomElement(['SENT', 'PAID', 'CANCELLED']))
                         ->setChrono($chrono)
-                        ->setCustomer($customer); 
+                        ->setCustomer($customer);
 
-                $chrono++;
+                    $chrono++;
 
-                $manager->persist($invoice);
+                    $manager->persist($invoice);
+                }
             }
         }
 
