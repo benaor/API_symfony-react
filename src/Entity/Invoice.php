f940c6d3a92e02ed0855a9665e1e30553bb35f34
benaor <benjamingirard25@gmail.com>
2020-11-16 22:47:42 +0100

    configure pagination from api_plateform

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -8,7 +8,12 @@ use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=InvoiceRepository::class)
- * @ApiResource
+ * @ApiResource(
+ *  attributes={
+ *      "pagination_enabled"=true,
+ *      "pagination_enabled_item_per_page"=20
+ * }
+ * )
  */
 class Invoice
 {
