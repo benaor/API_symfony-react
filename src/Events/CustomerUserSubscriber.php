33de3414fddb76cf2d77da56526eee679e427192
benaor <benjamingirard25@gmail.com>
2020-12-07 23:08:32 +0100

    Create CustomerUserSubscriber

---
+++ src/Events/CustomerUserSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\Customer;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+
+class CustomerUserSubscriber implements EventSubscriberInterface
+{
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['setUserForCustomer', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function setUserForCustomer(ViewEvent $event)
+    {
+        $customer = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if ($customer instanceof Customer && $method === "POST") {
+            // Choper l'utilisateur actuellement connecté
+            $user = $this->security->getUser();
+            // Assigner l'utilisateur au Customer qu'on est en train de créer
+            if ($user) {
+                $customer->setUser($user);
+            }
+        }
+    }
+}
