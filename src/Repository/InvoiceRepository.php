2cd7c89990b3738be3f05fe702589ae8702298d0
benaor <benjamingirard25@gmail.com>
2020-12-07 23:27:28 +0100

    create function FindNextChrono()

--- src/Repository/InvoiceRepository.php
+++ src/Repository/InvoiceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Invoice;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,6 +18,24 @@ class InvoiceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Invoice::class);
+    }
+
+
+    /**
+     * @return Invoice[] Returns an array of Invoice objects
+     */
+    public function findNextChrono(User $user)
+    {
+        return $this->createQueryBuilder('i')
+            ->select("chrono")
+            ->join("i.customer", "c")
+            ->where("c.user = :user")
+            ->setParameter("user", $user)
+            ->orderBy("i.chrono", "DESC")
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
     }
 
     // /**
