6eee8a30f8e08e5443eb74e555fd7cbe32f77f2a
benaor <benjamingirard25@gmail.com>
2020-12-29 23:37:02 +0100

    reorganization of javascripts files

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import { HashRouter, Switch, Route } from "react-router-dom";
+import CustomersPage from './pages/CustomersPage';
+
+// start the Stimulus application
+// import './bootstrap';
+
+console.log("Bonjour Benjamin");
+
+const App = () => {
+    return (
+        <HashRouter>
+            <Navbar />
+
+            <main className="container pt-5">
+                <Switch>
+                    <Route path="/customers" component={CustomersPage} />
+                    <Route path="/" component={HomePage} />
+                </Switch>
+            </main>
+        </HashRouter>
+    )
+}
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
