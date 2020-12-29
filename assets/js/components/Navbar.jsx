6cef8fd835e29f335c3c9f7beed73b42916d93b9
benaor <benjamingirard25@gmail.com>
2020-12-29 23:32:59 +0100

    Create Navbar component

---
+++ assets/js/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">SymReact</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarColor02">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Clients</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Factures</a>
+                    </li>
+                </ul>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <a href="" className="nav-link">
+                            Inscription
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="btn btn-light text-success">
+                            Connexion !
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#" className="btn btn-danger">
+                            Deconnexion !
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
